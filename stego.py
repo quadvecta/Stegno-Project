015034bba4d1b0ac94c2efb52107ff878c331281
Suhan Aktar <kidsuhanaktar@gmail.com>
2025-02-25 23:09:56 +0530

    Update stego.py
    Added AES Encryption Support

--- stego.py
+++ stego.py
@@ -1,64 +1,76 @@
 import cv2
 import os
+import base64
+from cryptography.fernet import Fernet
+import numpy as np
 
 
-image_path = r"D:\Programming\VS Code\Python\image.jpg" # Make sure to change the file location!
+image_path = r"D:\Programming\VS Code\Python\image.jpg" # Make sure to change the file location of the image!
+
 
 if not os.path.exists(image_path):
     print("Error: Image file not found at", image_path)
     exit()
 
-img = cv2.imread(image_path)
 
+img = cv2.imread(image_path)
 if img is None:
     print("Error: Failed to load the image. Check file format or permissions.")
     exit()
 
+
+key = Fernet.generate_key()
+cipher = Fernet(key)
+
+print("\n[NOTE] Keep this decryption key safe: ", key.decode())
+
+
 msg = input("Enter secret message: ")
 password = input("Enter a passcode: ")
 
-d = {chr(i): i for i in range(256)} 
-c = {i: chr(i) for i in range(256)}
+
+encrypted_msg = cipher.encrypt(msg.encode())  
+encoded_msg = base64.b64encode(encrypted_msg).decode()  
+
 
 height, width, _ = img.shape
-
-n = 0
-m = 0
-z = 0
-
-
-if len(msg) > height * width:
+if len(encoded_msg) > height * width:
     print("Error: Message too long to fit in the image.")
     exit()
 
 
-for i in range(len(msg)):
-    img[n, m, z] = d[msg[i]]
+n, m, z = 0, 0, 0
+for char in encoded_msg:
+    img[n, m, z] = ord(char)  
     m += 1
-    if m >= width:  
+    if m >= width:
         m = 0
         n += 1
     z = (z + 1) % 3  
 
 cv2.imwrite("encryptedImage.jpg", img)
-os.system("start encryptedImage.jpg")  # Open image on Windows using "start"
+os.system("start encryptedImage.jpg")  
 
+# Decryption Process
+print("\n--- Decryption Process ---")
+decryption_key = input("Enter the decryption key: ")
 
-message = ""
-n = 0
-m = 0
-z = 0
-
-pas = input("Enter passcode for decryption: ")
-
-if password == pas:
-    for i in range(len(msg)):
-        message += c[img[n, m, z]]
+if decryption_key.encode() == key: 
+    n, m, z = 0, 0, 0
+    extracted_chars = []
+    
+    for _ in range(len(encoded_msg)):
+        extracted_chars.append(chr(img[n, m, z])) 
         m += 1
         if m >= width:
             m = 0
             n += 1
         z = (z + 1) % 3
-    print("Decryption message:", message)
+    
+    
+    extracted_text = "".join(extracted_chars)
+    decrypted_msg = cipher.decrypt(base64.b64decode(extracted_text)).decode()
+
+    print("Decrypted Message:", decrypted_msg)
 else:
-    print("ERROR: Incorrect passcode. You are not authorized!")
+    print("ERROR: Incorrect decryption key. Access Denied!")
